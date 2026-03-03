fc0ad8d3d5f40a8fff0809bd2fc7863d020bb997
Dandi Agung Prasetyo <dandiagungprasetyo02@gmail.com>
2026-03-03 11:56:31 +0700

    Update Subkontraktor.jsx

--- apps/portal/src/pages/Subkontraktor.jsx
+++ apps/portal/src/pages/Subkontraktor.jsx
@@ -362,7 +362,7 @@ export default function Subkontraktor() {
                                                 <React.Fragment key={pm}>
                                                     <tr className="bg-slate-50/50 dark:bg-slate-800/30">
                                                         <td className="px-4 py-2 font-semibold text-primary text-xs tracking-wide" colSpan="5">
-                                                            <span className="material-icons text-xs align-middle mr-1">person</span> PM: {pm}
+                                                            <span className="material-icons text-xs align-middle mr-1">person</span> {pm}
                                                         </td>
                                                     </tr>
                                                     {Object.values(subconsMap).map(data => {
